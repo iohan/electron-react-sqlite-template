507f4e0382041347b251a681627dc3507acf5cd1
iohan <johan.ostling@gmail.com>
2022-11-21 11:01:40 +0100

    Added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-        extend: {},
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      soot: "#343b39",
+      spruce: "#4c5553",
+      clear: "#e1eae8",
+      cold: "#f0fdfa",
+      silver: "#bfd8ce",
+      wood: "#c49d88",
+      hellion: "#88c4ad",
+      white: "#fff",
     },
-    plugins: [],
+    extend: {},
+  },
+  plugins: [],
 };
