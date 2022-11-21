6543a182be8d3181df60ca1ad347ecee23632f55
iohan <johan.ostling@gmail.com>
2022-11-21 11:02:26 +0100

    Fixed Api Routes

--- public/electron.js
+++ public/electron.js
@@ -1,164 +1,140 @@
 const { app, BrowserWindow, session, ipcMain } = require("electron");
 const isDev = require("electron-is-dev");
+const { resolve } = require("path");
 const path = require("path");
 const sqlite3 = require("sqlite3");
 
 let mainWindow;
 
 const createWindow = () => {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: isDev
-                ? path.join(app.getAppPath(), "./public/preload.js")
-                : path.join(app.getAppPath(), "./build/preload.js"),
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: isDev ? path.join(app.getAppPath(), "./public/preload.js") : path.join(app.getAppPath(), "./build/preload.js"),
 
-            contextIsolation: true,
-        },
+      contextIsolation: true,
+    },
+  });
+
+  mainWindow.loadURL(isDev ? "http://localhost:3000/" : `file://${path.join(__dirname, "../build/index.html")}`);
+
+  if (isDev) {
+    mainWindow.webContents.on("did-frame-finish-load", () => {
+      mainWindow.webContents.openDevTools();
     });
-
-    mainWindow.loadURL(
-        isDev
-            ? "http://localhost:3000/"
-            : `file://${path.join(__dirname, "../build/index.html")}`
-    );
-
-    if (isDev) {
-        mainWindow.webContents.on("did-frame-finish-load", () => {
-            mainWindow.webContents.openDevTools();
-        });
-    }
+  }
 };
 
 // Setting the location for the userdata folder created by the Electron App
 app.setPath(
-    "userData",
-    isDev
-        ? path.join(app.getAppPath(), "userdata/") // Creates the folder 'userdata' where package.json is located
-        : path.join(process.resourcesPath, "userdata/") // Creates the folder 'userdata' in the resources folder
+  "userData",
+  isDev
+    ? path.join(app.getAppPath(), "userdata/") // Creates the folder 'userdata' where package.json is located
+    : path.join(process.resourcesPath, "userdata/") // Creates the folder 'userdata' in the resources folder
 );
 
 // When the app is ready to load
 app.whenReady().then(() => {
-    createWindow();
+  createWindow();
 
-    app.on("activate", () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow();
-        }
-    });
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
 });
 
 app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit();
-    }
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
 process.on("uncaughtException", (error) => {
-    console.log(`Exception: ${error} `);
-    if (process.platform !== "darwin") {
-        app.quit();
-    }
+  console.log(`Exception: ${error} `);
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
-const db = new sqlite3.Database(
-    isDev
-        ? path.join(__dirname, "../db/database.db")
-        : path.join(process.resourcesPath, "db/database.db"),
-    (err) => {
-        if (err) {
-            console.log(`Database Error: ${error}`);
-        } else {
-            console.log(`Database Loaded`);
-        }
-    }
-);
+/**
+ * ------------
+ * Database Functions
+ */
 
-const getContacts = () => {
-    return new Promise((resolve, reject) => {
-        db.all("SELECT * FROM contacts", [], (err, rows) => {
-            if (err) {
-                reject(err);
-            }
-            resolve(rows);
-        });
+const db = new sqlite3.Database(isDev ? path.join(__dirname, "../db/database.db") : path.join(process.resourcesPath, "db/database.db"), (err) => {
+  if (err) {
+    console.log(`Database Error: ${error}`);
+  } else {
+    console.log(`Database Loaded`);
+  }
+});
+
+const dbResponse = (promise) => {
+  return promise
+    .then((res) => {
+      return res;
+    })
+    .catch((err) => {
+      console.log("ERROR: " + err.message);
+      return false;
     });
 };
 
-const getBooks = () => {
-    return new Promise((resolve, reject) => {
-        db.all(
-            "SELECT b.id, b.title, b.year, c.category FROM books b INNER JOIN book_categories c ON b.category = c.id",
-            [],
-            (err, rows) => {
-                if (err) {
-                    reject(err);
-                }
-
-                resolve(rows);
-            }
-        );
+const dbAll = async (query, args) => {
+  const p = new Promise((resolve, reject) => {
+    db.all(query, args, (err, rows) => {
+      if (err) reject(err.message);
+      else resolve(rows);
     });
+  });
+
+  return await dbResponse(p);
 };
 
-ipcMain.handle("get-books", async (event, args) => {
-    console.log("Getting Books");
-    const sql = getBooks();
-    const result = await sql.then((rows) => {
-        console.log(rows);
-        return rows;
+const dbRun = async (query) => {
+  const p = new Promise((resolve, reject) => {
+    db.run(query, (err) => {
+      if (err) reject(err.message);
+      else resolve(true);
     });
+  });
 
-    return result;
+  return await dbResponse(p);
+};
+
+/**
+ * ------------
+ * API Calls
+ */
+
+ipcMain.handle("listContacts", async (event, args) => {
+  const result = await dbAll("SELECT * FROM contacts ORDER BY firstName asc", []);
+  return result;
 });
 
-ipcMain.handle("get-contacts", async (event, args) => {
-    console.log("Getting Contacts");
+ipcMain.handle("createContact", async (event, data) => {
+  const JSONdata = JSON.parse(data);
+  const sql = `INSERT INTO contacts(firstName, lastName, email, phone)
+              VALUES('${JSONdata.firstName}', '${JSONdata.lastName}', '${JSONdata.email}', '${JSONdata.phone}')`;
+  const result = await dbRun(sql);
+  console.log(result);
+  return result;
+});
 
-    const sql = getContacts();
+ipcMain.handle("updateContact", async (event, data) => {
+  const JSONdata = JSON.parse(data);
+  const sql = `UPDATE contacts SET
+              firstName = '${JSONdata.firstName}', lastName = '${JSONdata.lastName}', email = '${JSONdata.email}', phone = '${JSONdata.phone}'
+              WHERE id = '${JSONdata.id}'`;
+  const result = await dbRun(sql);
+  console.log(result);
+  return result;
+});
 
-    const result = await sql.then((rows) => {
-        console.log(rows);
-        return rows;
-    });
-
-    return result;
-
-    /*var promise = Promise.resolve("HEJ");
-
-    console.log(promise);
-
-    const result = await promise.then((value) => {
-        console.log(value);
-        return value;
-    });
-
-    console.log("Result:", result);
-    return result;*/
-
-    //getContacts();
-
-    /*let contacts;
-    await db.all("SELECT * FROM contacts", []).then((data) => {
-        contacts = data;
-    });
-    console.log(contacts);*/
-    /*let contactsResult;
-    const contacts = new Promise((resolve, reject) => {
-        db.all("SELECT * FROM contacts", [], (err, rows) => {
-            if (err) {
-                reject(err);
-            }
-            resolve(rows);
-            db.close();
-        });
-    });
-
-    contactsResult = contacts.then((result) => {
-        return result;
-    });
-    console.log(contactsResult);*/
-    //return [{ id: 1, name: "Johan", email: "greken" }];
+ipcMain.handle("deleteContact", async (event, id) => {
+  const result = await dbRun(`DELETE FROM contacts WHERE id = ${id}`);
+  console.log(result);
+  return result;
 });
--- public/preload.js
+++ public/preload.js
@@ -1,13 +1,8 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("api", {
-    getContacts: () => ipcRenderer.invoke("get-contacts"),
-    getBooks: () => ipcRenderer.invoke("get-books"),
-
-    testInvoke: (args) => ipcRenderer.invoke("test-invoke", args),
-    testSend: (args) => ipcRenderer.send("test-send", args),
-    testRecive: (callback) =>
-        ipcRenderer.on("test-receive", (event, data) => {
-            callback(data);
-        }),
+  listContacts: () => ipcRenderer.invoke("listContacts"),
+  createContact: (data) => ipcRenderer.invoke("createContact", data),
+  updateContact: (data) => ipcRenderer.invoke("updateContact", data),
+  deleteContact: (id) => ipcRenderer.invoke("deleteContact", id),
 });
